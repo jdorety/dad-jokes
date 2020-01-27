d30d3e715a9cb380aabbcf155839063eb81ad271
Jonathan Dorety <kyullog@gmail.com>
2020-01-26 16:44:15 -0800

    Can display joke from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const url = process.env.REACT_APP_DJ_API;
+
+  const [joke, setJoke] = useState("");
+
+  const getJoke = (endpoint = "") => {
+    axios({
+      method: "get",
+      url: `${url}${endpoint}`,
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(res => {
+        console.log(res);
+        const { data } = res;
+        setJoke(data.joke);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getJoke();
+  }, [url]);
+
   return (
     <div className="App">
+      <h2>{joke}</h2>
     </div>
   );
 }
