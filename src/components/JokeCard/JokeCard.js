b8d47bf2c5deaa04710b3e315208d5775844becf
Jonathan Dorety <kyullog@gmail.com>
2020-01-26 17:18:09 -0800

    Refactor / restyle JokeCard

--- src/components/JokeCard/JokeCard.js
+++ src/components/JokeCard/JokeCard.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 export default function JokeCard(props) {
   return (
     <div className="joke-card">
-      <h2>{props.joke}</h2>
+      <span>{props.joke}</span>
     </div>
   );
 }
